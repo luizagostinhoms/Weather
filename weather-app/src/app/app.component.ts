71fb37e6e72404d3d8880c0d4ba0cd7c57e12b63
luizagostinho <luizagostinhoms@gmail.com>
2019-10-31 22:16:18 -0300

    Add lógica para ver se pc ou mobile

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CityRequestWeather } from './weather/model/cityrequestweather';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 
 @Component({
@@ -11,12 +12,14 @@ export class AppComponent  {
   
   title = 'weather-app';
   cities: Array<CityRequestWeather>;
+  isMobile: boolean;
 
-  constructor() {
+  constructor(private deviceService: DeviceDetectorService) {
     this.cities = new Array<CityRequestWeather>();
     this.cities.push(new CityRequestWeather("Nuuk", "GL"));
     this.cities.push(new CityRequestWeather("Urubici", "BR"));
     this.cities.push(new CityRequestWeather("Nairobi", "KE"));
 
+    this.isMobile = this.deviceService.isMobile();
   }
 }
